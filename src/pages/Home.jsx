8325bfc625756ba45f86bccfef1504c971b7ff18
João Victor <joaovr.costa@gmail.com>
2022-09-26 16:21:05 -0300

    Adicionado nova faixa de produtos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,6 +47,294 @@ export function Home() {
           </div>
         </div>
       </div>
+
+      {/* product wrapper */}
+      <div className="max-w-[1280px] flex mx-auto pb-16">
+        {/* <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
+          Novidades
+        </h2> */}
+
+        {/* product grid */}
+        <div className="grid grid-cols-4 gap-6">
+          {/* single product */}
+          <div className="bg-white shadow rounded overflow-hidden group">
+            {/* product image */}
+            <div className="relative ">
+              <img src={clockOne} alt="" className="w-full" />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition cursor-pointer">
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-search"></i>
+                </a>
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            {/* product content */}
+            <div className="pt-4 pb-3 px-4">
+              <a href="#">
+                <h4 className="uppercase text-base mb-2 text-gray-800 font-bold">
+                Relógio SmartWatch W34 
+                </h4>
+              </a>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl font-semibold text-[#1768c5]">$45,00</p>
+                <p className="text-sm text-gray-400 line-through">$55,00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(150)</div>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="block w-full py-2 text-center text-white border border-[#001227] bg-[#001227] roundeed-b hover:bg-transparent hover:text-black"
+            >
+              Add to cart
+            </a>
+          </div>
+          {/* single product */}
+          <div className="bg-white shadow rounded overflow-hidden group">
+            {/* product image */}
+            <div className="relative ">
+              <img src={clockTwo} alt="" className="w-full" />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition cursor-pointer">
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-search"></i>
+                </a>
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            {/* product content */}
+            <div className="pt-4 pb-3 px-4">
+              <a href="#">
+                <h4 className="uppercase font-bold text-base mb-2 text-gray-800">
+                Relógio SmartWatch C74
+                </h4>
+              </a>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl font-semibold text-[#1768c5]">$45,00</p>
+                <p className="text-sm text-gray-400 line-through">$55,00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(150)</div>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="block w-full py-2 text-center text-white border border-[#001227] bg-[#001227] roundeed-b hover:bg-transparent hover:text-black"
+            >
+              Add to cart
+            </a>
+          </div>
+          {/* single product */}
+          <div className="bg-white shadow rounded overflow-hidden group">
+            {/* product image */}
+            <div className="relative ">
+              <img src={clockThree} alt="" className="w-full" />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition cursor-pointer">
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-search"></i>
+                </a>
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            {/* product content */}
+            <div className="pt-4 pb-3 px-4">
+              <a href="#">
+                <h4 className="uppercase font-bold text-base mb-2 text-gray-800">
+                Relógio SmartWatch K84
+                </h4>
+              </a>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl font-semibold text-[#1768c5]">$45,00</p>
+                <p className="text-sm text-gray-400 line-through">$55,00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(150)</div>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="block w-full py-2 text-center text-white border border-[#001227] bg-[#001227] roundeed-b hover:bg-transparent hover:text-black"
+            >
+              Add to cart
+            </a>
+          </div>
+          {/* single product */}
+          <div className="bg-white shadow rounded overflow-hidden group">
+            {/* product image */}
+            <div className="relative ">
+              <img src={clockFor} alt="" className="w-full" />
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition cursor-pointer">
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-search"></i>
+                </a>
+                <a
+                  href="#"
+                  className="text-white text-lg w-9 h-8 rounded-full bg-cyan-700 flex items-center justify-center hover:bg-gray-800 transition"
+                >
+                  <i className="fas fa-heart"></i>
+                </a>
+              </div>
+            </div>
+            {/* product content */}
+            <div className="pt-4 pb-3 px-4">
+              <a href="#">
+                <h4 className="uppercase font-bold text-base mb-2 text-gray-800">
+                Relógio SmartWatch W23
+                </h4>
+              </a>
+              <div className="flex items-baseline mb-1 space-x-2">
+                <p className="text-xl font-semibold text-[#1768c5]">$45,00</p>
+                <p className="text-sm text-gray-400 line-through">$55,00</p>
+              </div>
+              <div className="flex items-center">
+                <div className="flex gap-1 text-sm text-yellow-400">
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                  <span>
+                    <i className="fas fa-star">
+                      <img src={star} className="w-5 h-5" alt="" />
+                    </i>
+                  </span>
+                </div>
+                <div className="text-xs text-gray-500 ml-3">(150)</div>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="block w-full py-2 text-center text-white border border-[#001227] bg-[#001227] roundeed-b hover:bg-transparent hover:text-black"
+            >
+              Add to cart
+            </a>
+          </div>
+        </div>
+      </div>
+
       {/* product wrapper */}
       <div className="max-w-[1280px] flex mx-auto pb-16">
         {/* <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">
